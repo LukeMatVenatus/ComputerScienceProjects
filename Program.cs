13b1b8445f8e96978e7b34c7bb51856ffb676a8d
Luke Matakitoga <99804874+LukeMatVenatus@users.noreply.github.com>
2022-03-30 11:53:41 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter Rock, Paper or Scissors:");
+            string pChoiceStr = Console.ReadLine().ToLower();
+            int pChoice = pChoiceStr == "rock" ? 0 : (pChoiceStr == "paper" ? 1 : 2);
+            int cChoice = 
+        }
+
+      
+    }
+}
