4946bd224d45effe406c8337035f21cedccca02d
Luke Matakitoga <99804874+LukeMatVenatus@users.noreply.github.com>
2022-05-18 11:08:36 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Palindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a word:");
+            string originalWord = Console.ReadLine();
+            Console.WriteLine("Your word in reverse is {0}", ReverseString(originalWord));
+            Console.WriteLine("Your word with vowels as '?' is {0}", originalWord.Replace('a', '?').Replace('e', '?').Replace('i', '?').Replace('o', '?').Replace('u', '?'));
+            Console.WriteLine("Your word is {0}a palindrome", (ReverseString(originalWord.ToLower())==originalWord.ToLower())?"":"not ");
+        }
+
+        static string ReverseString(string s)
+        {
+            char[] array = s.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+    }
+}
